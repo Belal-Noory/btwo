239c9971e44ffbd0dd14b513e60fcc4dc90cfb83
Belal Noory <belalnoory2@gmail.com>
2023-02-09 20:00:08 +0430

    up

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,25 +2,24 @@ import React from 'react'
 import Header from '../Header/Header'
 import Navbar from './Navbar'
 import Team from '../Team/Team'
-import { OrganizationChart } from 'primereact/organizationchart';
-import map from "../Map/map.json";
+// import { OrganizationChart } from 'primereact/organizationchart';
+// import map from "../Map/map.json";
 import WorkArea from '../WorkArea';
-import Vision from "../Vision";
+// import Vision from "../Vision";
 import RecentActivity from '../RecentActivity';
 const Home = () => {
-  const kdz = map.features.filter(map => map.properties.provinceName === "Kunduz");
-  console.log(kdz);
-  const data = [{
-    label: 'Afghanistan',
-    expanded: true,
-    children: [
-      {
-        label: 'Kunduz',
-        expanded: true,
-        children: kdz.map(p => { return { label: p.properties.name } })
-      }
-    ]
-  }];
+  // const kdz = map.features.filter(map => map.properties.provinceName === "Kunduz");
+  // const data = [{
+  //   label: 'Afghanistan',
+  //   expanded: true,
+  //   children: [
+  //     {
+  //       label: 'Kunduz',
+  //       expanded: true,
+  //       children: kdz.map(p => { return { label: p.properties.name } })
+  //     }
+  //   ]
+  // }];
 
   return (
     <div className='m-0'>
@@ -29,10 +28,6 @@ const Home = () => {
       <RecentActivity/>
       <WorkArea/>
       <Team />
-      {/* <div className='text-center mt-4 overflow-x-auto p-4'>
-        <div className="font-bold text-3xl mb-3" style={{color:'var(--primaryColor)'}}>Where we work?</div>
-        <OrganizationChart value={data} aria-orientation='horizontal'></OrganizationChart>
-      </div> */}
     </div>
   )
 }
